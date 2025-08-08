3184bb5a1baabb65c11b36b0b004f45f2d0e9228
genie <beyondrealsmap@gmail.com>
2025-08-08 13:59:12 +0900

    next.config.js 파일에 api3.smap.site 추가 및 CSP 설정 업데이트

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -68,7 +68,8 @@ const nextConfig = {
       'app2.smap.site',
       'app.smap.site',
       'smap.site',
-      '118.67.130.71'
+      '118.67.130.71',
+      'api3.smap.site'
     ],
   },
   
@@ -184,7 +185,7 @@ const nextConfig = {
               "default-src 'self'",
               "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.map.naver.com *.googleapis.com *.gstatic.com *.google.com *.navercorp.com *.naver.net *.pstatic.net *.vercel-scripts.com *.vercel.com",
               "connect-src 'self' *.map.naver.com *.apigw.ntruss.com *.googleapis.com *.google.com *.navercorp.com *.naver.net *.pstatic.net *.vercel-scripts.com *.vercel.com https://api3.smap.site wss: ws: data: blob:",
-              "img-src 'self' data: blob: *.map.naver.com *.googleapis.com *.gstatic.com *.google.com *.navercorp.com *.naver.net *.pstatic.net *.vercel-scripts.com *.vercel.com",
+              "img-src 'self' data: blob: api3.smap.site https://api3.smap.site *.map.naver.com *.googleapis.com *.gstatic.com *.google.com *.navercorp.com *.naver.net *.pstatic.net *.vercel-scripts.com *.vercel.com",
               "style-src 'self' 'unsafe-inline' *.googleapis.com *.gstatic.com *.google.com accounts.google.com *.navercorp.com *.naver.net *.pstatic.net *.vercel-scripts.com *.vercel.com",
               "font-src 'self' *.gstatic.com *.googleapis.com *.navercorp.com *.naver.net *.pstatic.net *.vercel-scripts.com *.vercel.com",
               "frame-src 'self' *.google.com *.navercorp.com *.naver.net *.pstatic.net *.vercel-scripts.com *.vercel.com smap:",
